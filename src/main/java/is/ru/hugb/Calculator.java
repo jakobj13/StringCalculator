ed8bc4148c21cb808323d8d3262fd17823f2f4ac
jakobj13 <jakobj13@ru.is>
2017-10-15 22:10:19 +0000

    Java implementation for negative numbers implemented, TODO: TEST AND REFACTORING

--- src/main/java/is/ru/hugb/Calculator.java
+++ src/main/java/is/ru/hugb/Calculator.java
@@ -1,14 +1,29 @@
 package	is.ru.hugb;
+import java.util.ArrayList;import java.util.List;
 
 
 public	class	Calculator	{
 				
 				public	static int add(String numbers)	{
+					List negNumbers = new ArrayList();
 					if(numbers.equals(""))
 						return 0;
 					else{
 						if(numbers.contains("\n") | (numbers.contains(","))){
 							String stringnumb[] = numbers.split(",|\n");
+
+							for(String number : stringnumb){
+								int numberInt = toInt(number);
+								if(numberInt < 0){
+									negNumbers.add(number);
+								}
+							}
+							
+							if (negNumbers.size() > 0) {
+        						throw new RuntimeException("Negatives not allowed: " + negNumbers.toString().replace("[", "").replace("]", ""));
+
+
+    						}
 							return sum(stringnumb);
 						}
 
