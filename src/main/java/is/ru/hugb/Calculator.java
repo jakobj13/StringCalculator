aa677f677dc82e35f16bfa2043193578db3c4b85
jakobj13 <jakobj13@ru.is>
2017-10-15 20:58:23 +0000

    a little refactoring, sum function created

--- src/main/java/is/ru/hugb/Calculator.java
+++ src/main/java/is/ru/hugb/Calculator.java
@@ -9,11 +9,7 @@ public	class	Calculator	{
 					else{
 						if(numbers.contains(",")){
 							String stringnumb[] = numbers.split(",");
-							int total = 0;
-							for(String number : stringnumb){
-								total += toInt(number);
-							}
-							return total;
+							return sum(stringnumb);
 						}
 
 						return toInt(numbers);
@@ -22,6 +18,14 @@ public	class	Calculator	{
 
 				private static int toInt (String number){
 					return Integer.parseInt(number);
+				}
+
+				private static int sum(String [] numbers){
+					int total = 0;
+					for(String number : numbers){
+						total += toInt(number);
+					}
+					return total;
 				}
 
 				 public static void main(String[] args) {
